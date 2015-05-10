f710ef5f69618771743555e58a036e680410f881
aml030 <alorenz2016@kellogg.northwestern.edu>
2015-05-10 15:43:18 -0500

    rcav practice finished

--- app/controllers/calculations_controller.rb
+++ app/controllers/calculations_controller.rb
@@ -2,9 +2,24 @@ class CalculationsController < ApplicationController
   def instructions
   end
 
+  def square
+
+    @number = params[:the_number]
+    @square = @number.to_i**2
+
+  end
+
   def sqrt
+    @number = params[:the_number]
+    @sqrt = @number.to_i**0.5
+
   end
 
   def pmt
+    p = params[:p].to_i
+    r = (params[:r].to_f)/10000
+    n = params[:n].to_i
+
+    @payment = ((r)*p)/(1-(1+r)**-(n))
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
   get("/", { :controller => "calculations", :action => "instructions" })
+
+  get("/square/:the_number/", { :controller => "calculations", :action => "square"})
+
+  get("/sqrt/:the_number", { :controller => "calculations", :action => "sqrt"})
+
+  get("/payment/:r/:n/:p", { :controller => "calculations", :action => "pmt"})
+
+
 end
